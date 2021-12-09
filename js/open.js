de193b13437645b9fe811807fac0c4b3d27d536c
smappoh <94684049+smappoh@users.noreply.github.com>
2021-12-09 16:08:53 +0900

    Add files via upload

---
+++ js/open.js
@@ -0,0 +1,58 @@
+﻿ $(function(){
+
+        //1번째 이미지
+        $('.btn1').click(function(){
+          window.open("img/1.png","blank","width=1200, height=850")
+        });
+
+
+
+        //2번째 이미지
+        $('.btn2').click(function(){
+          window.open("img/2.png","blank","width=1200, height=850")
+        });
+
+
+
+
+        //3번째 이미지
+        $('.btn3').click(function(){
+          window.open("img/3.png","blank","width=612, height=612")
+        });
+
+
+
+        //4번째 이미지
+        $('.btn4').click(function(){
+          window.open("img/4.png","blank","width=1200, height=850")
+        });
+
+
+
+        //5번째 이미지
+        $('.btn5').click(function(){
+          window.open("img/5.png","blank","width=1200, height=850")
+        });
+
+
+
+
+        //6번째 이미지
+        $('.btn6').click(function(){
+          window.open("img/6.png","blank","width=1200, height=850")
+        });
+
+
+
+        //7번째 이미지
+        $('.btn7').click(function(){
+          window.open("img/7.png","blank","width=1200, height=850")
+        });
+
+
+
+        //8번째 이미지
+        $('.btn8').click(function(){
+          window.open("img/8.png","blank","width=768, height=1024")
+        });
+      });
